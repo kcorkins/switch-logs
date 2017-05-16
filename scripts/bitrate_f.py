8c583f1b37722ae3405410cc4476cf6df08daef0
Ken Corkins <kcorkins@lenovo.com>
2017-05-16 01:29:21 -0400

    first working ver of bitrate script

--- scripts/bitrate_f.py
+++ scripts/bitrate_f.py
@@ -5,16 +5,14 @@ import sys
 import re
 import xlsxwriter
 from br_functions import *
-from br_out_func import *
 
 
-# file_name1 = sys.argv[1]
-# print sys.argv[1]
-
-input_file = "IOM1-20170905_1555.txt"
-input_name = input_file.rsplit('.', 1)
-out_name = str(input_name[0]) + ".xlsx"
+# input_file = "IOM1-20170905_1555.txt"  # Manual override
+input_file = sys.argv[1]  # argv[1] is the name of the file to be parse
+input_name = input_file.rsplit('.', 1)  # remove the file name extension
+out_name = str(input_name[0]) + ".xlsx"  # add the Excel extension
 data = []
+valuelist = []
 InBits = ""
 OutBits = ""
 Port = ""
@@ -23,7 +21,7 @@ ws = createWorksheet(wb)
 print ws
 row = 1
 col = 0
-big_num = wb.add_format({'num_format': '0'})
+# big_num = wb.add_format({'num_format': '0'})
 
 with open(input_file, "r") as f:
     print f  # prints the name of the file
@@ -32,144 +30,28 @@ with open(input_file, "r") as f:
         record = parseBitRateCounters(str1)  # parses the counters for ea. port
         if record != None:  # if we found a line with real data
             # record looks like ['INTA2', '920', '931']
-            toExcel(record)
-            data.append(record[1])  # adds results to huge data string
+            data.append(record)  # adds results to huge data string
             # data looks like [['INTA1', '20', '31'], ['INTA2', '920', '931']]
             portnames = getportlist(record)  # get a list of ports
-            interfaceRows(record)
-            # howmanyrows = len(portnames)
-            # #print howmanyrows
-            # getrow = interfaceRows(record)  # defines row for Interface records
-            # inbits = intinbits(record)
-            # outbits = intoutbits(record)
-#data.sort(key=lambda tup: tup[0])
 
+# Now the records are in a huge string of strings called 'data'
 
-# print A1_inbits
-# print A1_outbits
-# print A2_inbits
-# print A2_outbits
-for item in A1_inbits:
-    ws.write(row, col, item, big_num)
-    ws.set_column(row, col, 18)
-    col += 1
-row += 1
-col = 0
-for item in A1_outbits:
-    ws.write(row, col, item, big_num)
-    ws.set_column(row, col, 18)
-    col += 1
-row += 1
-col = 0
-for item in A2_inbits:
-    ws.write(row, col, item, big_num)
-    ws.set_column(row, col, 18)
-    col += 1
-row += 1
-col = 0
-
-for item in A2_outbits:
-    ws.write(row, col, item, big_num)
-    ws.set_column(row, col, 18)
-    col +=1
-
-
-
-#for name in portnames:
-    #resultlist = buildstr(name, record)
-    #portvalues = addvalues(name, record)
-    #print resultlist
-    # print portvalues
-    #print valuelist
-
-
-# print record
-
-# for port in portnames:
-
-# for interface, inkbps, outkbps in (data):
-    # print interface, inkbps, outkbps
-    # ws.write(row, col,     interface)
-    # ws.write(row, col + 1, inkbps)
-    # ws.write(row, col + 2, outkbps)
-    # row += 1
-    # col += 2
-
-
-
-
-
-            #ws.write(getrow, col, record[0])  # Port Name
-            #print record[0], getrow, inbits
-
-            #print inbits
-            #ws.write(getrow, col, inbits)
-            #ws.write(getrow + 1, col + 1, record[1])  # InKbps
-            #ws.write(runrow + 2, col + 1, record[2])  # OutKbps
-            #col += 1
-            #row = 1
-        # print row, col
-            # ws.write(row, col + 2, record[2])
-
-
-
-
-
-
-
-
-
-        # ws1.write(1, 4, InBits)
-        # worksheet1.write(row, col + 2, OutBits, big_num)
-        #
-        # if str1.startswith("Port"):  # if the line starts with "Port"
-        #     colon = str1.find(":")  # Gives the index of the colon
-        #     # PortNum = str1[colon - 6:colon].strip()  # Give me "Port INTXX"
-        #     # Need to adjust for "EXT" ports
-        #     PortNum = str1[colon - 4:colon].strip()  # Give me "Port INTXX"
-        #     print PortNum
-        #
-        #     # find the first instance of "Kbps" (starts left->right)
-        #     In = str1.find("Kbps")
-        #     # Snag from after the colon to before Kbps
-        #     InBits = str1[colon + 1:In].strip()
-        #     Out = str1.rfind("Kbps")  # start from the right and finds Kbps
-        #     # snag from the end of end of InKbps to the Out (no Kbps)
-        #     OutBits = str1[In + 4:Out].strip()
-        #     # InBits = column[2]
-        #     # OutBits = column[3]
-        #     # print PortNum
-        #
-        #     # Copy the values to the cells
-        #     worksheet1.activate()
-        #     worksheet1.write(row, col, PortNum)
-        #     worksheet1.write(row, col + 1, InBits, big_num)
-        #     worksheet1.write(row, col + 2, OutBits, big_num)
-        #     # print "Row: " + str(row) + " Col: " + str(col)
-        #
-        #     row += 1
-        #     """
-        #     This section will place the next run to the right of the previous
-        #     run. Set the value to the highest port number you've captured.
-        #     If you don't want this, either comment the three lines out, or set
-        #     it to a value higher than you've captured (INTA15 for ex.)
-        #     """
-        #     # If we've gone through the captured ports,
-        #     # move a column to the right
-        #     if PortNum == "EXT4":  # Adjust based on number of ports captured
-        #         col += 2  # Move over three colums
-        #         row = 1  # Start from the top
-        #         # formatting crap
-        #         nnum_col = col + 1  # next_number
-        #         nnnum_col = nnum_col + 1  # next_next_number
-        #         worksheet1.set_column(nnum_col, nnnum_col, 18, big_num)
-        #         worksheet1.write(0, nnum_col, "InKbps", big_num)
-        #         worksheet1.write(0, nnnum_col, "OutKbps", big_num)
-        #     # worksheet2.activate()
-        #     # worksheet2.write(row, col, PortNum)
-        #     # worksheet2.write(row, col + 1, (InBits))
-        #     # worksheet2.write(row, col + 2, (OutBits))
-        #     # row += 1
+data.sort(key=lambda tup: tup[0])  # sorts 'data' by portname.
+for port in portnames:
+    valuelist = getresults(port)  # Doesn't do anything, just a call point
+    invalues(port, data)  # retrieves the InKbps values for the port
+    toExcel(ws, valuelist, row, col)  # copies the valuelist to wb
+    del valuelist[:]  # clears list
+    row += 1  # next row
+    outvalues(port, data)  # retrieves the OutKbps values for the port
+    toExcel(ws, valuelist, row, col)  #copies the valuelist to wb
+    del valuelist[:]  # clears list
+    row += 1
+print portnames  # Just a list of the ports we've gathered
 
 wb.close()
 f.closed
+
+if __name__ == "__main__":
+    input_file = sys.argv[1]
+    # First argument from command line. Name of the file to be parsed
--- scripts/br_functions.py
+++ scripts/br_functions.py
@@ -1,14 +1,14 @@
 import re
 import xlsxwriter
 
-# Print flag
-PRINT = 1
-
-
+valuelist = []
+item = []
+portlist = []
 
 def cleanup(line):
+    """does some basic restructuring of the incoming line of text"""
     raw_str = ''.join(line)  # convert each word in list to strings
-    str1 = stripESC(raw_str)
+    str1 = stripESC(raw_str)  # calls the function to strip ESC codes
     return str1
 
 
@@ -18,14 +18,12 @@ def stripESC(raw_str):
     str1 = ansi_escape.sub('', raw_str)
     return str1
 
-# def parseBitRateCounters(line, portName):
-
 
 def parseBitRateCounters(line):
     """
     Takes a line, return parsing of bitrate counters
     INPUT: Line from input file, String
-    Output: Array of format; portName, InBits, OutBits
+    Output: List, of format; [portName, InBits, OutBits]
     """
     if line.startswith("Port"):
         portName = re.search(r'INTA\d+|EXT\d+|\d+', line).group()
@@ -37,7 +35,7 @@ def parseBitRateCounters(line):
         result = [portName, InBits, OutBits]
         return result
 
-portlist = []
+
 def getportlist(record):
     """Get a list of the portnames in use"""
     portname = record[0]
@@ -46,50 +44,50 @@ def getportlist(record):
     return portlist
 
 
-valuelist = []
-item = []
-def buildstr(portname, data):
-    for name in portname:
-        valuelist.append(portname)
-        return valuelist
-
-
-def addvalues(name, data):
-    for i in data:
-        if name == data[0]:
-            #print data[0]
-            valuelist.append(data[1])
-
-
-def interfaceRows(record):
+def getresults(port):
     """
-    sets start row for each Interface
-    next row will be for InKbps
-    next row for OutKbps
+    Currently does nothing. Was my original list of list for-loop
+    takes portname, and list of results,
+    loops through each portname. Then requests the data points
     """
-    if record[0] == "INTA1":
-        runrow = 1
-    elif record[0] == "INTA2":
-        runrow = 4
-    elif record[0] == "INTA3":
-        runrow = 7
-    elif record[0] == "INTA4":
-        runrow = 10
-    return runrow
+    #for name in portname:
+
+    #valuelist.append(port)
+        #invalues(name, data)
+        # print valuelist
+        #del valuelist[:]  # clear list for next run - also deletes last time
+        # print valuelist
+    return valuelist
 
 
-def intinbits(record):
-    if record[0] == "INTA1":
-        A1inbits.append(record[1])
-        return A1inbits
+def invalues(port, data):
+    """
+    loops through data records
+    if the portname matches the portname in the data record
+    grab the data value and append it to the list
+    """
+    valuelist.append(port)  # start the row with portname. Col = A
+    valuelist.append("In Kbps")  # Col B for the type of info
+    for i, item in enumerate(data):
+        rec = data[i]  # pull the next data record
+        if port == item[0]:  # the first string in the rec is portname
+            valuelist.append(rec[1])  # rec[1] is the InKbps value
+    return valuelist
 
 
-def intoutbits(record):
-    if record[0] == "INTA1":
-        A1outbits.append(record[2])
-        return A1outbits
-# def PortArray():
-#     """Determines values of ports in output"""
+def outvalues(port, data):
+    """
+    loops through data records
+    if the portname matches the portname in the data record
+    grab the data value and append it to the list
+    """
+    valuelist.append(port)  # start the row with portname. Col = A
+    valuelist.append("Out Kbps")  # Col B for the type of info
+    for i, item in enumerate(data):
+        rec = data[i]  # pull the next data record
+        if port == item[0]:  # the first string in the rec is portname
+            valuelist.append(rec[2])  # rec[2] is the OutKbps value
+    return valuelist
 
 
 def createWorkbook(out_file):
@@ -99,114 +97,15 @@ def createWorkbook(out_file):
 
 def createWorksheet(wb):
     ws = wb.add_worksheet("Bitrates")  # would love to have timestamp
-    big_num = wb.add_format({'num_format': '0'})
-    #ws.set_column('B:C', 18, big_num)
-    # # Write header for Column Worksheet
-    # ws.write('A1', "Port")
-    # ws.write(0, 1, "InKbps", big_num)
-    # ws.write(0, 2, "OutKbps", big_num)
+    big_num = wb.add_format({'num_format': '0'})  # Not yet working
+    # ws.set_column(row, col, 18, big_num)
     return ws
 
-def toExcel(record):
-    for interface in (record):
-        # print interface
-    # ws.write(row, col,     interface)
-    # ws.write(row, col + 1, inkbps)
-    # ws.write(row, col + 2, outkbps)
-    # row += 1
-    # col += 2
-        return
 
- # ws.write(1,2,"oOct")
-#     row = int(rList[1])
-#     column = int(rList[0])
-#
-#     value1 = rList[2]
-#     counter2 = rList[3]
-#     value2 = rList[4]
-#     ws.write(column, row, value1)
-#     ws.write(column, row+1, value2)
-#     return
-
-
-
-#
-# def writeToExcel(rList, ws, option):
-    # """
-    # record = ['PortName', 'InKbps', 'OutKbps']
-    # want format;
-    # PortName
-    #     InKbps
-    #     OutKbps
-    # """#
-    # ws.write(1,2,"oOct")
-#     row = int(rList[0])
-#     column = int(rList[1])
-#     value1 = rList[2]
-#     ws.write(row, column, value1)
-#     return
-#
-#
-# def printList(rList):
-#     return
-#     for i in rList:
-#         print(i)
-#     return
-#
-#
-# def print1(string, flag):
-#     if flag:
-#         print(string)
-#
-#
-#
-#
-#
-# def getPortName(arr):
-#     return arr[0]
-#
-#
-# def getQueueNum(arr):
-#     return arr[3]
-#
-#
-# def getQueueType(arr):
-#     return arr[1]
-#
-#
-# def getQueueValue(arr):
-#     return arr[2]
-#
-#
-# def getNum(str1):
-#     """
-#     Input: String with two numbers in it separated
-#     Output: Group, two numbers
-#     """
-#     return re.search(r'\d+', str1).group()
-#
-#
-# def getNums(str1):
-#     return re.search(r'(\d+).* (\d+)', str1)
-#
-#
-# def getIntPort(str):
-#     # return re.search(r'(
-#     return
-#
-#
-# def isPort(port):
-#     """tells you if the item is a number"""
-#     if type(port) == int:
-#         return True
-#     else:
-#         return False
-#
-#
-# # line = "QoS Rate for port 4:"
-# line = "Tx Packets:                               4"
-# port = 0
-# parseQueueRate(line, port, 3)
-#
-# k = 5
-# print1(isPort(k), PRINT)
+def toExcel(ws, valuelist, row, col):
+    """Takes the string (valuelist) and writes it to worksheet"""
+    ws.set_column(row, col, 18)
+    for value in (valuelist):
+        ws.write(row, col, value)
+        col += 1
+    return  # implicit, but here for completeness
